cc91a75edbf9225f71e3a78c0b733b0dd929f5da
Andrew Snyder <ASnyder@minitab.com>
2022-02-08 08:16:11 -0500

    Revert "Update controller.js"
    This reverts commit f95eb48f5851c7a5d01614144ba9bac5efa01873.

--- controller.js
+++ controller.js
@@ -4,50 +4,6 @@ const k8s = require('@kubernetes/client-node');
 const kc = new k8s.KubeConfig();
 kc.loadFromCluster();
 const k8Core = kc.makeApiClient(k8s.CoreV1Api);
-k8Core.addInterceptor(options => {
- options.forever = true;
- options.timeout = 10000;
-});
-
-class WatchRequest {
-  webRequest(options) {
-    const {
-      ca, headers, method, qs, uri
-    } = options;
-    const url = new URL(uri);
-    const session = http2.connect(url, { ca });
-    let ping = null;
-    let error = '';
-    session.on('error', err => error += err);
-    session.on('close', () => clearInterval(ping));
-    const stream = session.request({ 
-      ...headers,
-      ':method': method, 
-      ':path'  : `${url.pathname}?${new URLSearchParams(qs)}`,
-      'accept' : 'application/json'
-    }, { 'endStream': false });
-    stream.setEncoding('utf8');
-    ping = setInterval(() => {
-      session.ping(error => {
-        if (error || stream.closed) {
-          clearInterval(ping);
-          if (!session.destroyed) {
-            session.destroy(error || 'stream was closed');
-          } else {
-            console.log(`session was already destroyed, ${error || 'stream was closed'}`);
-          }
-        }
-      });
-    }, 2000);
-    stream.on('error', () => {/* no opt this will allow session 'error' to be emitted instead of throwing an exception */});
-    stream.on('close', () => {
-      clearInterval(ping);
-      session.close();
-    });
-    stream.abort = () => {};
-    return stream;
-  }
-}
 let change = 0;
 let startTime;
 
@@ -55,32 +11,32 @@ const log = msg => console.log(`${(new Date() - startTime) / 1000.0} ${msg}`);
 
 const doWatch = async () => {
   let listMetadata = undefined;
-  log('Listing pods...');
-  const {
-    body: { items, metadata: { resourceVersion } }
-  } = await this.k8Core.listNamespacedPod(
-    'default',
-    false,
-    false,
-    undefined,
-    undefined,
-    'app=mssonline-api',
-    undefined,
-    undefined,
-    undefined,
-    30, /* We have seen some very slow listings these are very critical to the workload so anything slower than 30 seconds we gotta restart */ 
-  );
+  let pod = '';
+  do {
+    log('Listing pods...');
+    const {
+      body: { items: [{ metadata: { name: podA } }], metadata }
+    } = await k8Core.listNamespacedPod(
+      'default', false, false,
+      listMetadata && listMetadata._continue,
+      undefined,
+      'app=watch',
+      100
+    );
+    pod = podA;
+    listMetadata = metadata;
+  } while (listMetadata._continue);
   const { resourceVersion, selfLink } = listMetadata;
-  log(`Found ${items.length}`);
+  
   let timeout = null;
   log('Starting watch.');
-  const watchRequest = await new k8s.Watch(kc, new WatchRequest()).watch(
+  const watchRequest = await new k8s.Watch(kc).watch(
     selfLink,
     {
       resourceVersion,
-      timeoutSeconds: 210 // 3.5  minutes
+      timeoutSeconds: 6 * 60 // 6  minutes
     },
-    (type, { metadata: { name } }) => { log(type, name); },
+    () => { },
     e => {
       log(`Watch Shutdown ${e ? e : ''}`);
       clearTimeout(timeout);
@@ -89,9 +45,24 @@ const doWatch = async () => {
   );
   timeout = setTimeout(() => {
     log('!!! Manually aborting watch !!!!!');
-    watchRequest.destroy();
+    watchRequest.abort();
     doWatch();
-  }, 211 * 1000); // if we get to 3 minutes 31 seconds we need to kill it and restart something didn't close correctly, thanks azure load balancer
+  }, 8 * 60 * 1000); // if we get to 8 minutes we need to kill it and restart something didn't close correctly
+
+  setTimeout(async () => {
+    log(`Patching ${pod}`);
+    await k8Core.patchNamespacedPod(pod, 'default',
+      [
+        {
+          op   : 'replace',
+          path : '/metadata/labels/change',
+          value: (change += 1).toString()
+        }
+      ],
+      undefined, undefined, undefined, undefined,
+      { headers: { 'Content-Type': 'application/json-patch+json' } }
+    );
+  }, 2 * 1000); // 2 seconds
 };
 
 const delay = t => new Promise(resolve => setTimeout(resolve, t));
